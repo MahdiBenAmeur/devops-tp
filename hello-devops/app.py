daf7b0dad865473a8eba4d35d49b205b65e709ff
mahdiBA <mahdi.benameur.98@gmail.com>
2025-03-13 14:17:19 +0100

    added new endpoints

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,1 +1,14 @@
-print ("Hello , CI/CD!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+@app.route('/status')
+def hello():
+    return "waw a status endpoint so cooll"
+
+if __name__ == "__main__":
+    app.run()
